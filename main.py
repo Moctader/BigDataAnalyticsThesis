99d667aa9e53768028c986273bde88e47b6e3c24
Md Golam Moctader <golam.moctader@gmail.com>
2023-11-24 23:48:26 +0200

    combine tiles

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-from preprocessing import samples, setting_values
-
+from preprocessing import samples, combine_tiles, nearest_tile_dict
+import glob
+import os
 
 
 tiles_template = "/Users/moctader/Thesis/{t}/{z}/{x}/{y}.png"
 zoom_level = 10
-t_values = ['landsat', 'aem_apparent_resistivity', 'aem_imaginary_component',
-            'aem_real_component', 'elev_10m', 'elev_10m_aspect', 'elev_10m_hillshade',
-            'elev_10m_slope', 'TPI', 'corine_land_cover']
+t_values = glob.glob('/Users/moctader/Thesis/*')
 
+unique_tiles = set(tile for tile, info in nearest_tile_dict.items())
+sorted_tiles = sorted(nearest_tile_dict.items(), key=lambda x: x[1]['distance'])
 
 for i in range(samples.shape[0]):
     for t_value in t_values:
-        try:
-           tz, tx, ty =setting_values.display_pixel(samples.iloc[i], zoom_level, tiles_template, t_value, window_size=50)
-        except FileNotFoundError:
-            print(f"No valid {t_value} found for sample {i}  ------> {tz}/{tx}/{ty}  ")
-            pass
+        if not t_value.endswith('.zip'):
+            t_value = os.path.splitext(os.path.basename(t_value))[0]
+
+            try:
+                cropped_image, tz, tx, ty = combine_tiles(samples.iloc[i], zoom_level, tiles_template, t_value,
+                                                                nearest_tile_dict)
+
+            except FileNotFoundError:
+                print(f"No valid {t_value} found for sample {i}  ------> {tz}/{tx}/{ty}  ")
+                pass
--- preprocessing.py
+++ preprocessing.py
@@ -7,7 +7,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import os
-
+import math
 
 output_root_directory = "/Users/moctader/Thesis_code/output4/"
 points = gpd.read_file("/Users/moctader/Thesis_code/GTK_ASsoil_obs.csv")
@@ -19,114 +19,150 @@ samples = gpd.GeoDataFrame(
 ).to_crs("WGS84")
 
 
+tile_list = [(point.x, point.y) for point in samples['geometry']]
+
+def calculate_distance(tile1, tile2):
+    x1, y1 = tile1
+    x2, y2 = tile2
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
+def find_nearest_tile(tile, tile_list):
+    distances = {}
+
+    for other_tile in tile_list:
+        if other_tile != tile:
+            distance = calculate_distance(tile, other_tile)
+            distances[other_tile] = distance
+
+    # Find the nearest tile
+    nearest_tile = min(distances, key=distances.get)
+    return nearest_tile, distances[nearest_tile]
 
 
 TILE_SIZE = 256
-class setting_values:
-    
-    def project(p, zoom):
-        lon, lat = p.geometry.x, p.geometry.y
-        
-        siny = np.sin(lat * np.pi / 180)
-        siny = min(max(siny, -0.9999), 0.9999)
-        
-        x = TILE_SIZE * (0.5 + lon / 360)
-        y = TILE_SIZE * (0.5 - np.log((1 + siny) / (1 - siny)) / (4 * np.pi))
-        
-        scale = 2**zoom
-
-        tx = x * scale // TILE_SIZE
-        ty = y * scale // TILE_SIZE
-
-        px = x * scale % TILE_SIZE // 1
-        py = y * scale % TILE_SIZE // 1
-
-        return (int(zoom), int(tx), int(ty)), (px, py)
 
 
+def project(p, zoom):
+    lon, lat = p.geometry.x, p.geometry.y
 
-    def display_pixel(p, zoom, tileset, t_value, window_size=50):
-        (tz, tx, ty), (px, py) = setting_values.project(p, zoom=zoom)
+    siny = np.sin(lat * np.pi / 180)
+    siny = min(max(siny, -0.9999), 0.9999)
 
-        fname = tileset.format(t=t_value, z=tz, x=tx, y=ty)
-        im = Image.open(fname)
+    x = TILE_SIZE * (0.5 + lon / 360)
+    y = TILE_SIZE * (0.5 - np.log((1 + siny) / (1 - siny)) / (4 * np.pi))
 
-        # Get a list of all neighboring tiles
-        neighboring_tiles = []
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] 
-        
-        for dx, dy in directions:
+    scale = 2**zoom
+
+    tx = x * scale // TILE_SIZE
+    ty = y * scale // TILE_SIZE
+
+    px = x * scale % TILE_SIZE // 1
+    py = y * scale % TILE_SIZE // 1
+
+    return (int(zoom), int(tx), int(ty)), (px, py)
+
+
+nearest_tile_dict = {}
+
+for tile in tile_list:
+    nearest_tile, distance = find_nearest_tile(tile, tile_list)
+    nearest_tile_dict[tile] = {'nearest_tile': nearest_tile, 'distance': distance}
+
+
+def calculate_neighboring_tiles(tx, ty, radius=1):
+    neighboring_tiles = []
+    for dx in range(-radius, radius + 1):
+        for dy in range(-radius, radius + 1):
             neighboring_tiles.append((tx + dx, ty + dy))
-            
-        
-        # Load all neighboring tiles
-        tile_images = []
-        for nx, ny in neighboring_tiles:
-            fname = tileset.format(t=t_value, z=tz, x=nx, y=ny)
-            try:
-                tile_image = Image.open(fname)
-                tile_images.append(tile_image)
-            except FileNotFoundError:
-                pass
-                
-                
-                
-        # Each tile has the same dimensions
-        width, height = tile_images[0].size
-
-        # Create a new image with dimensions based on the number of tiles
-        combined_image = Image.new('RGB', (len(neighboring_tiles) * width, height))
-
-        # Paste each tile into the combined image at its respective position
-        for i, tile_image in enumerate(tile_images):
-            combined_image.paste(tile_image, (i * width, 0)) 
-                
-      
-        # Ensure the cropped window is centered around the point
-        left = max(px - window_size // 2, 0)
-        top = max(py - window_size // 2, 0)
-        right = min(px + window_size // 2, len(neighboring_tiles) * width)
-        bottom = min(py + window_size // 2, height)
-        
-        # Calculate the adjustment to center the point
-        adjustment_x = (window_size - (right - left)) // 2
-        adjustment_y = (window_size - (bottom - top)) // 2
-
-        # Apply the adjustment
-        left -= adjustment_x
-        right += adjustment_x
-        top -= adjustment_y
-        bottom += adjustment_y
-
-        # Crop the image
-        combined_image_crop = combined_image.crop((left, top, right, bottom))
-
-        # Save the cropped image in a subdirectory based on the variable 't'
-        output_directory = os.path.join(output_root_directory, t_value)
-        os.makedirs(output_directory, exist_ok=True)
-        output_path = os.path.join(output_directory, f"image_{p.name}.png")
-        combined_image_crop.save(output_path)
-
-        # Display the cropped image with the point at the center
-        # Original Combined Image
-        plt.subplot(1, 3, 1)
-        cropped_image_resized = combined_image.resize((1024, 1024))  
-        plt.imshow(cropped_image_resized, interpolation="lanczos")
-        plt.plot(px, py, "*", c="c", markersize=12)
-        plt.title("Original Combined Image")
+    return neighboring_tiles
 
 
-        # Cropped Image with Center Marker
-        plt.subplot(1, 3, 2)
-        plt.imshow(im, interpolation="lanczos")
-        plt.plot(px, py, "*", c="c", markersize=12)
-        plt.title("Mapped Image")
 
-        # Combined Image Crop
-        plt.subplot(1, 3, 3)
-        plt.imshow(combined_image_crop, interpolation="lanczos")
-        plt.plot(window_size // 2, window_size // 2, "*", c="c", markersize=12)
-        plt.title("Mapped Image Cropped from Combined Image")
 
-        plt.show()
-        return tz, tx, ty  
+def combine_tiles(p, zoom, tile_template, t_value, nearest_tile_dict):
+    (tz, tx, ty), (px, py) = project(p, zoom=zoom)
+
+    # Get a list of all neighboring tiles
+    radius = 1 
+    neighboring_tiles = calculate_neighboring_tiles(tx, ty, radius)
+
+   # Adjust the range as needed
+    extra_left_tiles = [(tx-i, ty) for i in range(4, 8)]  
+    neighboring_tiles.extend(extra_left_tiles)
+
+
+    # Load neighboring tiles and find the closest tile
+    tile_images = {}
+    closest_tile = None
+    closest_distance = float('inf')
+
+    for tile in neighboring_tiles:
+        nx, ny = tile
+        fname = tile_template.format(t=t_value, z=tz, x=nx, y=ny)
+        try:
+            tile_image = Image.open(fname)
+            tile_images[tile] = tile_image
+        except FileNotFoundError:
+            pass
+
+    # Check if there are tiles available before proceeding
+    if not tile_images:
+        print("No tiles available.")
+        return None, None, None, None
+
+    # Assuming each tile has the same dimensions
+    width, height = list(tile_images.values())[0].size
+
+    # Create a new image with dimensions based on the number of tiles
+    combined_image = Image.new('RGB', (3 * width, 3 * height))
+
+    # Paste each tile into the combined image at its respective position
+    # Adjust position based on central tile
+    for tile, tile_image in tile_images.items():
+        nx, ny = tile
+        x, y = nx - tx + 1, ny - ty + 1  
+        combined_image.paste(tile_image, (x * width, y * height))
+
+ 
+    center_x, center_y = px, py
+    crop_size = 50
+
+    # Calculate the crop box
+    left = max(0, center_x - crop_size // 2)
+    top = max(0, center_y - crop_size // 2)
+    right = min(combined_image.width, left + crop_size)
+    bottom = min(combined_image.height, top + crop_size)
+
+    # Crop the region of interest
+    cropped_image = combined_image.crop((left, top, right, bottom))
+
+    # plt.figure(figsize=(18, 5))  # Adjust the figure size as needed
+
+    # # Original Combined Image
+    # plt.subplot(1, 2, 1)
+
+    # plt.imshow(combined_image, interpolation="lanczos")
+    # plt.plot(px, py, "*", c="r", markersize=12)
+    # plt.text(px, py, f'({px}, {py})', color="r", fontsize=8, verticalalignment='bottom',
+    #          horizontalalignment='right')
+    # plt.title("Original Combined Image")
+    
+    
+    # plt.subplot(1, 2,2)
+    # # 50x50 Image Centered around px, py
+    # plt.imshow(cropped_image, interpolation="lanczos")
+    # plt.plot(center_x - left, center_y - top, "*", c="r", markersize=12)
+    # plt.text(center_x - left, center_y - top, f'({center_x}, {center_y})', color="r", fontsize=8,
+    #          verticalalignment='bottom', horizontalalignment='right')
+    # plt.title("50x50 Image Centered around px, py")
+
+    # plt.show()
+
+    # Save the cropped image in a subdirectory based on the variable 't'
+    output_directory = os.path.join(output_root_directory, t_value)
+    os.makedirs(output_directory, exist_ok=True)
+    output_path = os.path.join(output_directory, f"image_{p.name}.png")
+    cropped_image.save(output_path)
+
+    return cropped_image, tz, tx, ty
